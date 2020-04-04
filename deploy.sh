72c21c33631bde8582485a47f72bd5c1a5d61bb9
Drew Ferris <c_dferris@groupon.com>
2020-04-04 13:11:21 -0700

    configuring deployment

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+docker build -t drew2222222/react-signup-form:latest -t drew2222222/react-signup-form:$SHA -f ./client/Dockerfile ./client
+
+docker push drew2222222/react-signup-form:latest
+
+docker push drew2222222/react-signup-form:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=drew2222222/react-signup-form:$SHA
